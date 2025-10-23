777d1986ebffad71442fe566955709589786ff75
Vaibhav Giri <girivaibhav918@gmail.com>
2025-10-23 19:45:13 +0530

    done with change

--- .github/workflows/extract.py
+++ .github/workflows/extract.py
@@ -1,1 +1,13 @@
+import pandas as pd
+
 print("Extract Data FRom MYSQL")
+
+data = {
+    "id": [1,2,3],
+    "name": ["avnish","kiran","vaibhav"],
+    "salary": [10000,20000,30000]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
